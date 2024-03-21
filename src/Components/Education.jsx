97b83745235220c1ba46b4f119bfb115d90c4520
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-21 12:47:49 +0300

    let see if this are done this time

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -3,28 +3,30 @@ import React from "react";
 
 const Education = () => {
   return (
-    <div className="education-grid" id="Education">
-      <div className="education-cell">
-        <h3>Bsc.Mathematics &Computer Science</h3>
-        <p>Technical University of Mombasa</p>
-        <p>2020 - 2024</p>
+    <section id="Education">
+      <div className="education-grid">
+        <div className="education-cell">
+          <h3>Bsc.Mathematics &Computer Science</h3>
+          <p>Technical University of Mombasa</p>
+          <p>2020 - 2024</p>
+        </div>
+        <div className="education-cell">
+          <h3>Software Engineering</h3>
+          <p>Alx-Africa & Holberton University</p>
+          <p>2023 - 2024</p>
+        </div>
+        <div className="education-cell">
+          <h3>CyberSecurity</h3>
+          <p>Cisco Networking Academy & Ingressive for Good</p>
+          <p>2022 - 2023</p>
+        </div>
+        <div className="education-cell">
+          <h3>Health Information Systems</h3>
+          <p>Nairobi University & Kenya Ministry of Health Virtual Academy</p>
+          <p>2022 - 2023</p>
+        </div>
       </div>
-      <div className="education-cell">
-        <h3>Software Engineering</h3>
-        <p>Alx-Africa & Holberton University</p>
-        <p>2023 - 2024</p>
-      </div>
-      <div className="education-cell">
-        <h3>CyberSecurity</h3>
-        <p>Cisco Networking Academy & Ingressive for Good</p>
-        <p>2022 - 2023</p>
-      </div>
-      <div className="education-cell">
-        <h3>Health Information Systems</h3>
-        <p>Nairobi University & Kenya Ministry of Health Virtual Academy</p>
-        <p>2022 - 2023</p>
-      </div>
-    </div>
+    </section>
   );
 };
 
