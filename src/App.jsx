483ea59b32a1db19ee9d574a0eb7ba2baa1128bd
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-21 03:10:35 +0300

    weuh sijui anything

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@
 import React from "react";
 
 import About from "./Components/About";
+import Education from "./Components/Education";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
---
+++ src/Components/Education.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+/* add an education section to your portfolio site.*/
+const Education = () => {
+  return (
+    <section className="padding" id="education">
+      <div style={{ backgroundColor: "white", padding: "2rem" }}>
+        <h2>Education</h2>
+        <div>
+          <h3>Bsc.Mathematics &Computer Science</h3>
+          <p>Technical University of Mombasa</p>
+          <p>2020 - 2024</p>
+        </div>
+        <div>
+          <h3>Software Engineering</h3>
+          <p>Alx-Africa & Holberton University</p>
+          <p>2023 - 2024</p>
+        </div>
+        <div>
+            <h3>CyberSecurity</h3>
+            <p>Cisco Networking Academy & Ingressive for Good</p>
+            <p>2022 - 2023</p>
+        </div>
+        <div>
+            <h3>Health Information Systems</h3>
+            <p>Nairobi University & Kenya Ministry of Health Virtual Academy</p>
+            <p>2022 - 2023</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
